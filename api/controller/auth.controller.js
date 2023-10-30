b684c55019fb98b08e896280e7e1b122cd38fd18
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-10-30 20:44:14 +0000

    create middleware

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,5 +1,6 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from '../utils/error.js';
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -17,8 +18,6 @@ export const signup = async (req, res, next) => {
         })
     }catch(err){
         next(err);
-        
-        
         }
     }
     
