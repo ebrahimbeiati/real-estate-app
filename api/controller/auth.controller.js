e6a8c8742f16649862e617b48481c4cb1b8002b7
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-10-31 14:45:49 +0000

    create sign api

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from '../utils/error.js';
+import  Jwt  from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -20,4 +21,31 @@ export const signup = async (req, res, next) => {
         next(err);
         }
     }
-    
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) {
+          return res.status(404).json({
+            success: false,
+            message: "User not found",
+          });
+        }
+        const isPasswordCorrect = bcryptjs.compareSync(
+          password,
+          validUser.password
+        );
+        if (!isPasswordCorrect) {
+            return res.status(400).json({
+                success: false,
+                message: "Wrong password",
+            })
+        }
+        const token = Jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const { password: hashedPassword, ...others } = validUser._doc;
+        res.cookie('jwToken', token, { httpOnly: true }).Status(200).json(others);
+            
+            } catch (err) {
+            next(err);
+            }
+        };
