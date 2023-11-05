04ebfabcfec2c884aa858bff1984444b1192c037
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-11-05 23:33:19 +0000

    create listing api rout

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import path from "path";
+import listingRouter from "./routes/listing.route.js";
 dotenv.config();
 
 mongoose
@@ -30,6 +31,8 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use('/api/listing', listingRouter);
+
 
 
 
