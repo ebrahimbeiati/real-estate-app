8f3f241bf6820f36653d80c6ac370da98f07789c
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-11-06 23:28:00 +0000

    create getuser listings

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcryptjs';
 import User from '../models/user.model.js';
+import Listing from '../models/listing.model.js';
 
 
 
@@ -43,3 +44,17 @@ export const sinOut = async (req, res, next) => {
     next(err)
   }
 }
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (err) {
+      next(err);
+    }
+  } else {
+    return next(errorHandler(401, 'You can see your own list'));
+  }
+};
+
+
