b47ec388abe46e95127bc6f2034bf71d401b424c
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-11-07 13:16:56 +0000

    create update listing api route

--- api/controller/listing.controller.js
+++ api/controller/listing.controller.js
@@ -1,5 +1,5 @@
 import Listing from "../models/listing.model.js";
-import { errorHandler } from "../utils/error.js";
+import {errorHandler}  from "../utils/error.js";
 
 
 export const createListing = async (req, res, next) => {
@@ -22,4 +22,34 @@ export const deleteListing = async (req, res, next) => {
     } catch (err) {
         next(err);
     }
+}
+
+export const updateListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(404, "Listing not found!"));
+    }
+
+    if (req.user.id !== listing.userRef) {
+      return next(errorHandler(401, "You can only update your own listings!"));
+    }
+
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    res.status(200).json(updatedListing);
+  } catch (error) {
+    if (error.name === 'CastError' && error.kind === 'ObjectId') {
+        return res.status(400).json({
+            success: false,
+            statusCode: 400,
+            message: "Invalid listing ID",
+        });
+    }
+      next(error);
+  }
 }
