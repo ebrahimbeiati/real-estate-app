e63196288742eba9eb6e555285b2940b339724be
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-11-08 21:11:45 +0000

    add onsubmit to search

--- api/controller/listing.controller.js
+++ api/controller/listing.controller.js
@@ -73,36 +73,39 @@ export const getListings = async (req, res, next) => {
     const limit = parseInt(req.query.limit) || 9;
     const startIndex = parseInt(req.query.startIndex) || 0;
     let offer = req.query.offer;
-    if (offer === undefined || offer === 'false') {
-      offer = {$in: [false, true]};
+    if (offer === undefined || offer === "false") {
+      offer = { $in: [false, true] };
     }
-    if (furnished === undefined || furnished === 'false') {
-      furnished = {$in: [false, true]};
+    let furnished = req.query.furnished; // Define furnished
+    if (furnished === undefined || furnished === "false") {
+      furnished = { $in: [false, true] };
     }
-    if (parking === undefined || parking === ' false') {
-      parking = {$in: [false, true]};
+    let parking = req.query.parking; // Define parking
+    if (parking === undefined || parking === "false") {
+      parking = { $in: [false, true] };
     }
     let type = req.query.type;
-    if (type === undefined|| type === 'all') {
-      type = {$in: ['sale', 'rent']};
+    if (type === undefined || type === "all") {
+      type = { $in: ["sell", "rent"] };
     }
-    const searchTerm = req.query.searchTerm || '';
-    const sort = req.query.sort || 'createdAt';
-    const order = req.query.order || 'desc';
+    const searchTerm = req.query.searchTerm || "";
+    const sort = req.query.sort || "createdAt";
+    const order = req.query.order || "desc";
 
     const listings = await Listing.find({
-      title: { $regex: searchTerm, $options: 'i' },
+      title: { $regex: searchTerm, $options: "i" },
       type,
       offer,
       parking,
-      furnished,
-    }).sort(
-      { [sort]: order }
-    ).limit(limit).skip(startIndex);
-    return res.status(200).json(listings);
+      furnished, // Use the defined variable
+    })
+      .sort({ [sort]: order })
+      .limit(limit)
+      .skip(startIndex);
 
-  }
-  catch (err) {
+    return res.status(200).json(listings);
+  } catch (err) {
     next(err);
   }
-}
+};
+
