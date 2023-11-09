d1fb00cb42a92450ca62fd66d57d7b834d42dbed
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-11-09 19:19:31 +0000

    add reinstalling

--- api/index.js
+++ api/index.js
@@ -16,6 +16,7 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+const __dirname = path.resolve();
 
 
 
@@ -32,6 +33,12 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname, "client/dist")));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+})
 
 
 
