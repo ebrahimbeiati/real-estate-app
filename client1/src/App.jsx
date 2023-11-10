2af46dd506ea393d463b72b8dde4b47d5f99f94b
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-11-10 19:43:13 +0000

    fixing private error

--- client1/src/App.jsx
+++ client1/src/App.jsx
@@ -1,3 +1,46 @@
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Home from "./pages/Home";
+// import Profile from "./pages/Profile";
+// import SignUp from "./pages/SignUp";
+// import SignIn from "./pages/SignIn";
+// import About from "./pages/About";
+// import CreateListing from "./pages/CreateListing";
+// import Header from "./components/Header";
+// import PrivateRoute from "./components/PrivateRoute";
+// import UpdateListing from "./pages/UpdateListing";
+// import Listing from "./pages/Listing";
+// import Contact from "./components/Contact";
+// import Search from "./pages/Search";
+
+// const App = () => {
+//   return (
+//     <BrowserRouter>
+//       <Header />
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <PrivateRoute>
+//           <Route path="/profile" element={<Profile />} />
+//           <Route path="/create-listing" element={<CreateListing />} />
+//           <Route
+//             path="/update-listing/:listingId"
+//             element={<UpdateListing />}
+//           />
+//         </PrivateRoute>
+
+//         <Route path="/sign-up" element={<SignUp />} />
+//         <Route path="/sign-in" element={<SignIn />} />
+//         <Route path="/about" element={<About />} />
+//         <Route path="/listing/:listingId" element={<Listing />} />
+//         <Route path="/contact/:landlordId" element={<Contact />} />
+//         <Route path="/search" element={<Search />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// };
+
+// export default App;
+
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
@@ -18,24 +61,27 @@ const App = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route element={<PrivateRoute />}>
-          <Route path="/profile" element={<Profile />} />
-        </Route>
-        <Route path="/create-listing" element={<CreateListing />} />
-        <Route path="/update-listing/:listingId" element={<UpdateListing />} />
-
+        <Route
+          path="/profile"
+          element={<PrivateRoute element={<Profile />} />}
+        />
+        <Route
+          path="/create-listing"
+          element={<PrivateRoute element={<CreateListing />} />}
+        />
+        <Route
+          path="/update-listing/:listingId"
+          element={<PrivateRoute element={<UpdateListing />} />}
+        />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/about" element={<About />} />
         <Route path="/listing/:listingId" element={<Listing />} />
         <Route path="/contact/:landlordId" element={<Contact />} />
         <Route path="/search" element={<Search />} />
-
       </Routes>
     </BrowserRouter>
   );
 };
 
 export default App;
-
-
