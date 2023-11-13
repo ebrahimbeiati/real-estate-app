c08f216f70cf388466930fced49c866a815a0637
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-11-13 12:38:36 +0000

    edit

--- client1/vite.config.js
+++ client1/vite.config.js
@@ -5,11 +5,14 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   server: {
     proxy: {
-      '/api':{
-        target: 'http://localhost:3000',
-        secure:false,
-      } ,
+      "/api": {
+        target: "http://localhost:3000",
+        secure: false,
       },
     },
+  },
   plugins: [react()],
-})
+  build: {
+    outDir: "dist",
+  },
+});
