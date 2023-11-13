610cfae308d49fe78f90c7d348583d225e130b56
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-11-13 11:58:41 +0000

    edit

--- api/index.js
+++ api/index.js
@@ -1,11 +1,64 @@
+// import express from "express";
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+// import userRouter from "./routes/user.route.js";
+// import authRouter from "./routes/auth.route.js";
+// import cookieParser from "cookie-parser";
+// import path from "path";
+// import listingRouter from "./routes/listing.route.js";
+// dotenv.config();
+
+// mongoose
+//   .connect(process.env.MONGO)
+//   .then(() => {
+//     console.log("Connected to MongoDB!");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+// const __dirname = path.resolve();
+
+
+
+// const app = express();
+
+// app.use(express.json());
+
+// app.use(cookieParser());
+
+// app.listen(3000, () => {
+//   console.log("Server is running on port 3000!");
+// });
+
+// app.use("/api/user", userRouter);
+// app.use("/api/auth", authRouter);
+// app.use('/api/listing', listingRouter);
+
+// app.use(express.static(path.join(__dirname, "client/dist")));
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+// })
+
+
+// app.use((err, req, res, next) => {
+//   const statusCode = err.statusCode || 500;
+//   const message = err.message || "Internal Server Error";
+//   return res.status(statusCode).json({
+//     success: false,
+//     statusCode,
+//     message,
+//   });
+// });
+
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
 import path from "path";
-import listingRouter from "./routes/listing.route.js";
 dotenv.config();
 
 mongoose
@@ -16,9 +69,8 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
 const __dirname = path.resolve();
-
-
 
 const app = express();
 
@@ -32,16 +84,9 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
-app.use('/api/listing', listingRouter);
+app.use("/api/listing", listingRouter);
 
-app.use(express.static(path.join(__dirname, "client/dist")));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-})
-
-
-
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
@@ -56,3 +101,4 @@ app.use((err, req, res, next) => {
     message,
   });
 });
+
