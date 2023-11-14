22e8f18f4ba7261d1c37333b3e90325a60cbf777
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-11-14 19:43:06 +0000

    edit endpoint

--- api/index.js
+++ api/index.js
@@ -78,8 +78,8 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000!");
+app.listen(4000, () => {
+  console.log("Server is running on port 4000!");
 });
 
 app.use("/api/user", userRouter);
