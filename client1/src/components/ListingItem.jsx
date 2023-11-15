2957a665ae4eef546aed6eafb470f9957836a7f2
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-11-15 11:34:58 +0000

    edit2

--- client1/src/components/ListingItem.jsx
+++ client1/src/components/ListingItem.jsx
@@ -1,51 +1,70 @@
-import { Link } from "react-router-dom"
-import { MdLocationOn } from 'react-icons/md'
+import { Link } from "react-router-dom";
+import { MdLocationOn } from "react-icons/md";
+import PropTypes from "prop-types"; 
 
+ListingItem.propTypes = {
+  listing: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    // Add other necessary PropTypes based on your 'listing' object structure
+    // For example:
+    name: PropTypes.string.isRequired,
+    address: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    imageUrls: PropTypes.arrayOf(PropTypes.string).isRequired,
+    regularPrice: PropTypes.number.isRequired,
+    discountedPrice: PropTypes.number,
+    type: PropTypes.string.isRequired,
+    bed: PropTypes.number.isRequired,
+    bath: PropTypes.number.isRequired,
+    parking: PropTypes.bool.isRequired,
+    furnished: PropTypes.bool.isRequired,
+    offer: PropTypes.bool,
+    // Add more as per your structure
+  }).isRequired,
+};
 
-
-
-
-const ListingItem = ({listing}) => {
+export default function ListingItem({ listing }) {
   return (
-    <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden w-full sm:w-[330px] rounded-lg">
+    <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]">
       <Link to={`/listing/${listing._id}`}>
         <img
-          src={listing.imageUrls[0]}
-          alt="listing"
-          className="w-full h-[300px] sm:h-[200px] hover:scale-105
-              transition-scale duration-300 object-cover"
+          src={
+            listing.imageUrls[0] ||
+            "https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/Sales_Blog/real-estate-business-compressor.jpg?width=595&height=400&name=real-estate-business-compressor.jpg"
+          }
+          alt="listing cover"
+          className="h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300"
         />
-        <div className=" p-3 flex flex-col gap-2 w-full ">
-          <h1 className="truncate text-lg font-semibold ">{listing.title}</h1>
+        <div className="p-3 flex flex-col gap-2 w-full">
+          <p className="truncate text-lg font-semibold text-slate-700">
+            {listing.name}
+          </p>
           <div className="flex items-center gap-1">
-            <MdLocationOn className="text-red-600 h-4 w-4" />
-            <p className="text-gray-600 truncate">{listing.address}</p>
+            <MdLocationOn className="h-4 w-4 text-green-700" />
+            <p className="text-sm text-gray-600 truncate w-full">
+              {listing.address}
+            </p>
           </div>
-          <p className="text-gray-500 text-sm line-clamp-2">
+          <p className="text-sm text-gray-600 line-clamp-2">
             {listing.description}
           </p>
-                  <p className="text-slate-500 font-semibold mt-2">
-                      £
+          <p className="text-slate-500 mt-2 font-semibold ">
+            $
             {listing.offer
               ? listing.discountedPrice.toLocaleString("en-US")
-                          : listing.regularPrice.toLocaleString("en-US")}
+              : listing.regularPrice.toLocaleString("en-US")}
             {listing.type === "rent" && " / month"}
-        
           </p>
-          <div className="flex gap-4">
-            <div>
-               <p className="text-xs font-bold">
-              {listing.bedrooms > 1
-                ? `${listing.bedrooms} Bedrooms`
-                : "1 Bedroom"}
-            </p>
+          <div className="text-slate-700 flex gap-4">
+            <div className="font-bold text-xs">
+              {listing.bed > 1
+                ? `${listing.bed} beds `
+                : `${listing.bed} bed `}
             </div>
-            <div>
-               <p className="text-xs font-bold">
-              {listing.bathrooms > 1
-                ? `${listing.bathrooms} Bathrooms`
-                : "1 Bathroom"}
-              </p>
+            <div className="font-bold text-xs">
+              {listing.bath > 1
+                ? `${listing.bath} baths `
+                : `${listing.bath} bath `}
             </div>
           </div>
         </div>
@@ -53,5 +72,3 @@ const ListingItem = ({listing}) => {
     </div>
   );
 }
-
-export default ListingItem
