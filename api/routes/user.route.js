cb9d9c4688b69b940d35d6de81c091823511f624
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-11-15 11:19:59 +0000

    edit1

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,20 +3,11 @@ import { updateUser, deleteUser,getUserListings, getUser } from '../controller/u
 import { verifyToken } from '../utils/verifyUser.js';
 
 
-
-
 const router = express.Router();
 router.post('/update/:id', verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
 router.get('/listings/:id', verifyToken, getUserListings);
 router.get("/:id", verifyToken, getUser);
-
-
-
-
-
-
-
 
 
 export default router;
--- client1/src/components/ListingItem.jsx
+++ client1/src/components/ListingItem.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom"
-import {MdLocationOn} from 'react-icons/md'
+import { MdLocationOn } from 'react-icons/md'
+
+
 
 
 
